f810dc49a063b324df66fae17f11ff1517733af9
jason van pelt <jvanpelt@gmail.com>
2025-12-14 02:48:04 -0600

    Rename Scoring button to 'Cube & Scoring Guide'

--- js/version.js
+++ js/version.js
@@ -8,7 +8,7 @@
  * - Build tracking
  */
 
-export const VERSION = 'v4.53.3';
+export const VERSION = 'v4.53.4';
 
 // Convert version to a cache-busting number
 // Format: MAJOR.MINOR.PATCH -> numeric timestamp-like value
